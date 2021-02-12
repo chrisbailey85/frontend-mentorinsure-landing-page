50253325f8c14ba2ea329306f998fce216e04717
Chris Bailey <chris85bailey@aol.co.uk>
2021-02-12 16:09:28 +0000

    first commit

---
+++ dist/main.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav-list-link');
+
+
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('open');
+})
+
+navLinks.forEach(link => link.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('open');
+}
+))
